94779bcdedda8d23accadcea32261d2e8cf2b392
semdinsp <scott.sproule@ficonab.com>
2021-12-05 23:34:48 +0800

    first version for bisa
    up# Changes to be committed:

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -15,33 +15,37 @@ const ContactPage = ({
         <meta name="description" content={"Contact page of " + site.siteMetadata.description} />
       </Helmet>
       <div className="two-grids -contact">
-        <div className="post-thumbnail" style={{backgroundImage: `url('/assets/alexander-andrews-HgUDpaGPTEA-unsplash.jpg')`, marginBottom: 0}}>
-          <h1 className="post-title">Get in Touch</h1>
-          <p>Let me help you kick start your next project &rarr;</p>
-        </div>
-        <div>
-          <form className="form-container" action="https://sendmail.w3layouts.com/SubmitContactForm" method="post">
-            <div>
-              <label htmlFor="w3lName">Name</label>
-              <input type="text" name="w3lName" id="w3lName"/>
-            </div>
-            <div>
-              <label htmlFor="w3lSender">Email</label>
-              <input type="email" name="w3lSender" id="w3lSender"/>
-            </div>
-            <div>
-              <label htmlFor="w3lSubject">Subject</label>
-              <input type="text" name="w3lSubject" id="w3lSubject"/>
-            </div>
-            <div>
-              <label htmlFor="w3lMessage">Message</label>
-              <textarea name="w3lMessage" id="w3lMessage"></textarea>
-            </div>
-            <div style={{display: "flex", justifyContent: "flex-end"}}>
-              <input type="submit" className="button -primary" style={{marginRight: 0}} />
-            </div>
-          </form>
-        </div>
+      <div className="post-thumbnail" style={{backgroundImage: `url('/assets/catnip-lg.jpg')`, marginBottom: 0}}>
+        <h1 className="post-title">Get in Touch</h1>
+        <p>Let us help with sales of your product &rarr;</p>
+      </div>
+      <div>
+        <form className="form-container" name="contact-bisa" data-netlify="true"  data-netlify-honeypot="bot-field" action="/success"  method="POST">
+          <div>  
+            <input type="hidden" name="bot-field" />
+            <input type="hidden" name="form-name" value="contact-bisa" />
+          </div>
+          <div>
+            <label htmlFor="name">Name
+            <input type="text" name="name" id="name" /></label>
+          </div>
+          <div>
+            <label htmlFor="email">Email
+            <input type="email" name="email" id="sender" /></label>
+          </div>
+          <div>
+            <label htmlFor="subject">Subject
+            <input type="text" name="subject" id="subject" /></label>
+          </div>
+          <div>
+            <label htmlFor="message">Message
+            <textarea name="message" id="message"> </textarea></label>
+          </div>
+          <div style={{display: "flex", justifyContent: "flex-end"}}>
+            <button type="submit" className="button -primary" style={{marginRight: 0}}>Send</button>
+          </div>
+        </form>
+      </div>
       </div>
     </Layout>
   )
